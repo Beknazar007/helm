7c38851cb25d2e935dfb8a345f14b38e6cdf2a51
nurizaesenbaeva <85342800+nurizaesenbaeva@users.noreply.github.com>
2021-12-20 22:06:56 +0600

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,137 +1,8 @@
 #!/bin/bash
-echo "------------------------------------AMBASSADOR------------------------------------"
-
-####--------CHECKING HELM EXISTENCE------------
-
-if [[ -n $(helm version | grep "version.BuildInfo" ) ]]
-then
-    echo " "
-else
-    echo "-----------Installing HELM---------"
-    wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
-    tar xvf helm-v3.6.0-linux-amd64.tar.gz
-    sudo mv linux-amd64/helm /usr/local/bin
-    ./get_helm.sh
-    rm helm-v3.6.0-linux-amd64.tar.gz
-fi
-
-###----CHECKING CERT-MANAGER NAMESPACE EXISTENCE--- 
-
-
-if [[ -n $(kubectl get ns | grep "ambassador" )  ]]
-then
-    echo "namespace ambassador already exists"
-else
-    kubectl create ns ambassador
-fi
-kubectl apply -f https://www.getambassador.io/yaml/aes-crds.yaml 
 
 
 
 
-
-#--------CHECKING HELM REPO EXISTENCE-------
-
-
-if [[ -n $(helm repo list | grep datawire)  ]]
-then
-    echo "repo  already exists"
-else
-    helm repo add datawire https://www.getambassador.io 
-fi
-#-----HELM INSTALLING AND UPGRADING PART----
-cd ambassador
-
-helm repo update
-helm upgrade --install ambassador-aes1 datawire/ambassador -n ambassador \
--f values.yaml \
---atomic \
---timeout 10m \
---debug
-
-kubectl apply -f global.yaml -f tls.yaml
-
-
-
-#!/bin/bash
-echo "------------------------------------CERT-MANAGER------------------------------------"
-
-####--------CHECKING HELM EXISTENCE------------
-
-if [[ -n $(helm version | grep "version.BuildInfo" ) ]]
-then
-    echo " "
-else 
-    echo "-----installing HELM"
-    wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
-    tar xvf helm-v3.6.0-linux-amd64.tar.gz
-    sudo mv linux-amd64/helm /usr/local/bin
-    rm helm-v3.6.0-linux-amd64.tar.gz
-fi
-
-###----CHECKING CERT-MANAGER NAMESPACE EXISTENCE--- 
-
-
-if [[ -n $(kubectl get ns | grep "cert-manager" )  ]]
-then
-    echo "namespace cert-manager already exists"
-else
-    kubectl create ns cert-manager
-fi
-
-
-cd ../cert-manager ##DIRECTORY IN WHICH THERE ARE CREDENTIALS.JSON AND VALUES.YAML
-#------------CREATING SECRET FROM CREDENTIALS.JSON-----
-
-if [[-n $(kubectl get secret | grep "prod-cert-manager" ) ]]
-then
-    echo "secret prod-cert-manager alreay exists"
-else
-    kubectl create secret generic prod-cert-manager --from-file=credentials.json -n cert-manager
-fi
-
-kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
-
-#--------CHECKING HELM REPO EXISTENCE-------
-
-if [[ -n $(helm repo list | grep jetstack)  ]]
-then
-    echo "repo already exists"
-else
-    helm repo add jetstack https://charts.jetstack.io 
-fi
-#-----HELM INSTALLING AND UPGRADING PART----
-
-helm repo update 
-helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager \
---version v0.13.1 \
---atomic \
---wait 
-
-#-----CHECKING LINKERD EXISTENCE-------
-
-if [[ -n $(linkerd version | grep "Client version") ]]
-then
-    echo " "
-else
-    echo "---------INSTALLING LINKERD----------"
-    curl -fsL https://run.linkerd.io/install | sh
-    export PATH=$PATH:$HOME/.linkerd2/bin
-    linkerd check --pre
-    linkerd install | kubectl apply -f -
-    echo "Linkerd processes are running. Wait 10s"
-    sleep 10s
-fi
-
-echo "----ANNOTATING WITH LINKERD-----------------------"
-kubectl get deploy -o yaml -c cert-manager | linkerd inject - | kubectl apply -f -
-
-
-kubectl apply -n cert-manager -f clusterissuer-staging.yaml 
-kubectl apply -n cert-manager -f clusterissuer-prod.yaml
-
-
-#!/bin/bash
 echo "------------------------------------EXTERNAL-DNS------------------------------------"
 
 ####--------CHECKING HELM EXISTENCE------------
@@ -214,21 +85,135 @@ fi
 echo "----ANNOTATING WITH LINKERD-----------------------"
 kubectl get deploy -o yaml external-dns  -n external-dns | linkerd inject - | kubectl apply -f -
 
-#!/bin/bash
-echo "------------------------------------RELOADER------------------------------------"
 
-###--------checking helm existence----
 
-if [[ -n $(helm version | grep "version.BuildInfo" ) ]]
+
+
+
+
+
+
+echo "------------------------------------CERT-MANAGER------------------------------------"
+
+
+
+###----CHECKING CERT-MANAGER NAMESPACE EXISTENCE--- 
+
+
+if [[ -n $(kubectl get ns | grep "cert-manager" )  ]]
+then
+    echo "namespace cert-manager already exists"
+else
+    kubectl create ns cert-manager
+fi
+
+
+cd ../cert-manager ##DIRECTORY IN WHICH THERE ARE CREDENTIALS.JSON AND VALUES.YAML
+#------------CREATING SECRET FROM CREDENTIALS.JSON-----
+
+if [[-n $(kubectl get secret | grep "prod-cert-manager" ) ]]
+then
+    echo "secret prod-cert-manager alreay exists"
+else
+    kubectl create secret generic prod-cert-manager --from-file=credentials.json -n cert-manager
+fi
+
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
+
+#--------CHECKING HELM REPO EXISTENCE-------
+
+if [[ -n $(helm repo list | grep jetstack)  ]]
+then
+    echo "repo already exists"
+else
+    helm repo add jetstack https://charts.jetstack.io 
+fi
+#-----HELM INSTALLING AND UPGRADING PART----
+
+helm repo update 
+helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager \
+--version v0.13.1 \
+--atomic \
+--wait 
+
+#-----CHECKING LINKERD EXISTENCE-------
+
+if [[ -n $(linkerd version | grep "Client version") ]]
 then
     echo " "
 else
-    echo "-----installing HELM"
-    wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
-    tar xvf helm-v3.6.0-linux-amd64.tar.gz
-    sudo mv linux-amd64/helm /usr/local/bin
-    rm helm-v3.6.0-linux-amd64.tar.gz
+    echo "---------INSTALLING LINKERD----------"
+    curl -fsL https://run.linkerd.io/install | sh
+    export PATH=$PATH:$HOME/.linkerd2/bin
+    linkerd check --pre
+    linkerd install | kubectl apply -f -
+    echo "Linkerd processes are running. Wait 10s"
+    sleep 10s
 fi
+
+echo "----ANNOTATING WITH LINKERD-----------------------"
+kubectl get deploy -o yaml -c cert-manager | linkerd inject - | kubectl apply -f -
+
+
+kubectl apply -n cert-manager -f clusterissuer-staging.yaml 
+kubectl apply -n cert-manager -f clusterissuer-prod.yaml
+
+
+
+
+
+
+
+
+
+echo "------------------------------------AMBASSADOR------------------------------------"
+
+
+
+###----CHECKING CERT-MANAGER NAMESPACE EXISTENCE--- 
+
+
+if [[ -n $(kubectl get ns | grep "ambassador" )  ]]
+then
+    echo "namespace ambassador already exists"
+else
+    kubectl create ns ambassador
+fi
+kubectl apply -f https://www.getambassador.io/yaml/aes-crds.yaml 
+
+
+
+
+
+#--------CHECKING HELM REPO EXISTENCE-------
+
+
+if [[ -n $(helm repo list | grep datawire)  ]]
+then
+    echo "repo  already exists"
+else
+    helm repo add datawire https://www.getambassador.io 
+fi
+#-----HELM INSTALLING AND UPGRADING PART----
+cd ambassador
+
+helm repo update
+helm upgrade --install ambassador-aes1 datawire/ambassador -n ambassador \
+-f values.yaml \
+--atomic \
+--timeout 10m \
+--debug
+
+kubectl apply -f global.yaml -f tls.yaml
+
+
+
+
+
+
+echo "------------------------------------RELOADER------------------------------------"
+
+
 
 
 #-------checking reloader namespaces existence-
@@ -257,3 +242,4 @@ helm install reloader stakater/reloader \
 -n reloader \
 --atomic \
 --wait
+
